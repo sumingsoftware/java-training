7e28efc01cb5704b0be471f23887eef597d9e027
suming <sumingabc@126.com>
2019-07-10 16:24:18 +0800

    upload Eurkea server1

---
+++ eclipse-workspace1/EureKaProvider1/src/main/java/com/huawei/imc/provider/UserController.java
@@ -0,0 +1,17 @@
+package com.huawei.imc.provider;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class UserController {
+	@Autowired
+	private RestTemplate restTemplate;
+	
+	@GetMapping("/getUser")
+	public String getUser() {
+		return "I am user list. form provider 1";
+	}
+}
